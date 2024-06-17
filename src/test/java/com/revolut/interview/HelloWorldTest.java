355ccbda4bfeb03ef66141b16c04dfd3d695bcc9
h.moradi <hadi000333@gmail.com>
2024-06-17 13:41:12 +0330

    update hello world test

---
+++ src/test/java/com/revolut/interview/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.revolut.interview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+    @Test
+    void shouldPrintHelloWorld() {
+        HelloWorld.main(null);
+    }
+}
